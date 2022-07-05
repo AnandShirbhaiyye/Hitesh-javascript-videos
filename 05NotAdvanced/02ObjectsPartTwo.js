1363eb821ec997bd0f1582d394c754bb4f0344cb
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-07-05 18:20:41 +0530

    added lectures of 35 to 43 hitesh choudhary

---
+++ 05NotAdvanced/02ObjectsPartTwo.js
@@ -0,0 +1,17 @@
+var User = {
+    name: "",
+    getUserName : function(){
+        console.log(`User name is : ${this.name}`);
+    },
+};
+
+var hitesh = Object.create(User);
+console.log(hitesh);
+hitesh.name = "hitesh Choudhary"
+hitesh.getUserName();
+
+var sam = Object.create(User, {
+    name: {value: "samy"},
+    courseCourt: {value: 3},
+});
+sam.getUserName();
---
+++ 05NotAdvanced/03functions.js
@@ -0,0 +1,6 @@
+(function sayHello(){
+    console.log("I say hello");
+    console.log("I again say Hello");
+})();
+
+//sayHello();
